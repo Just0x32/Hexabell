8c35fdd0e7d43174c79e61faa865a6f3b79d5ff7
Just <jalui@mail.ru>
2022-01-21 23:04:09 +0300

    Move sizing, design and location members from View to ViewModel.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,130 +24,36 @@ namespace Hexabell
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        #region [ Fields and Properties ]
-        ViewModel viewModel;
-
-        public bool[] IsTaskEnabled { get => viewModel.IsTaskEnabled; }
-
-        public Brush[] ButtonBackgroundColors { get; private set; }
-
-        public Brush[] ButtonBorderBrushColors { get; private set; }
-
-        private readonly Brush disabledTaskButtonBackground = new SolidColorBrush(Color.FromArgb(93, 135, 206, 250));       // LightSkyBlue
-
-        private readonly Brush disabledTaskButtonBorderBrush = new SolidColorBrush(Color.FromArgb(120, 135, 206, 250));       // LightSkyBlue
-
-        private readonly Brush enabledTaskButtonBackground = new SolidColorBrush(Color.FromArgb(93, 255, 182, 193));        // LightPink
-
-        private readonly Brush enabledTaskButtonBorderBrush = new SolidColorBrush(Color.FromArgb(120, 255, 182, 193));         // LightPink
-        
-        private readonly Brush isMouseOverTaskButtonBorderBrush = new SolidColorBrush(Color.FromArgb(200, 135, 206, 250));         // LightSkyBlue
-        public Brush IsMouseOverTaskButtonBorderBrush { get => isMouseOverTaskButtonBorderBrush; }
-
-        public double HexagonStrokeThickness { get; private set; } = 3;
+        private static readonly int taskQuantity = 6;
+        public ViewModel viewModel = new ViewModel(taskQuantity);
 
         private Dictionary<Button, int> indexFromButton = new Dictionary<Button, int>();
         private Dictionary<int, Button> buttonFromIndex = new Dictionary<int, Button>();
 
-        private readonly GridPosition basicPolygonGridPosition = new GridPosition(2, 3);
-        private readonly GridPosition[] taskButtonGridPositions = new GridPosition[]
-        {
-            new GridPosition(2, 5),
-            new GridPosition(1, 4),
-            new GridPosition(1, 2),
-            new GridPosition(2, 1),
-            new GridPosition(3, 2),
-            new GridPosition(3, 4)
-        };
-
-        private PointCollection hexagonPoints = new PointCollection
-        {
-            { new Point(50, 0) },
-            { new Point(150, 0) },
-            { new Point(200, 87) },
-            { new Point(150, 173) },
-            { new Point(50, 173) },
-            { new Point(0, 87) },
-        };
-        public PointCollection HexagonPoints
-        {
-            get => hexagonPoints;
-            private set
-            {
-                hexagonPoints = value;
-                OnPropertyChanged();
-            }
-        }
-
         public int ForMouseClickTime { get; private set; } = 200;       // From settings
-        #endregion
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = viewModel;
 
-            int taskQuantity = 6;
-            viewModel = new ViewModel(taskQuantity);
+            InitializeTaskButtonIndexesAndColors(taskQuantity);
 
-            InitializeShapes();
-
-            void InitializeShapes()
+            void InitializeTaskButtonIndexesAndColors(int taskQuantity)
             {
-                InitializeBasicPolygon();
-                InitializeTaskButtonIndexesAndColors(taskQuantity);
+                buttonFromIndex.Add(0, FirstTaskButton);
+                buttonFromIndex.Add(1, SecondTaskButton);
+                buttonFromIndex.Add(2, ThirdTaskButton);
+                buttonFromIndex.Add(3, FourthTaskButton);
+                buttonFromIndex.Add(4, FifthTaskButton);
+                buttonFromIndex.Add(5, SixthTaskButton);
 
-                HexagonSize = 50;                                          // From settings
-                HexagonInterval = 20;                                       //
-
-                void InitializeBasicPolygon()
+                for (int i = 0; i < taskQuantity; i++)
                 {
-                    BasicPolygon.Fill = disabledTaskButtonBackground;
-                    BasicPolygon.Stroke = disabledTaskButtonBorderBrush;
-                    BasicPolygon.Points = HexagonPoints;
-                }
-
-                void InitializeTaskButtonIndexesAndColors(int taskQuantity)
-                {
-                    ButtonBackgroundColors = new Brush[taskQuantity];
-                    ButtonBorderBrushColors = new Brush[taskQuantity];
-
-                    buttonFromIndex.Add(0, FirstTaskButton);
-                    buttonFromIndex.Add(1, SecondTaskButton);
-                    buttonFromIndex.Add(2, ThirdTaskButton);
-                    buttonFromIndex.Add(3, FourthTaskButton);
-                    buttonFromIndex.Add(4, FifthTaskButton);
-                    buttonFromIndex.Add(5, SixthTaskButton);
-
-                    for (int i = 0; i < taskQuantity; i++)
-                    {
-                        ButtonBackgroundColors[i] = disabledTaskButtonBackground;
-                        ButtonBorderBrushColors[i] = disabledTaskButtonBorderBrush;
-
-                        indexFromButton.Add(buttonFromIndex[i], i);
-                        SetTaskButtonColor(buttonFromIndex[i]);
-                        buttonFromIndex[i].Content = (i + 1).ToString() + "0:00";               // Change to real values
-                    }
+                    indexFromButton.Add(buttonFromIndex[i], i);
+                    buttonFromIndex[i].Content = (i + 1).ToString() + "0:00";               // Change to real values
                 }
             }
-        }
-
-        private void SetTaskButtonColor(Button taskButton)
-        {
-            int index = indexFromButton[taskButton];
-
-            if (IsTaskEnabled[index])
-            {
-                ButtonBackgroundColors[index] = enabledTaskButtonBackground;
-                ButtonBorderBrushColors[index] = enabledTaskButtonBorderBrush;
-            }
-            else
-            {
-                ButtonBackgroundColors[index] = disabledTaskButtonBackground;
-                ButtonBorderBrushColors[index] = disabledTaskButtonBorderBrush;
-            }
-
-            taskButton.Background = ButtonBackgroundColors[index];
-            taskButton.BorderBrush = ButtonBorderBrushColors[index];
         }
 
         private void ChangeTaskState(Button taskButton)
@@ -155,7 +61,9 @@ namespace Hexabell
             int index = indexFromButton[taskButton];
 
             viewModel.ChangeTaskState(index);
-            SetTaskButtonColor(taskButton);
+            //SetTaskButtonColor(taskButton);
+
+            viewModel.HexagonSize += 5;                   // Debug
         }
 
         private void ChangeTaskOptions(Button taskButton)
@@ -163,135 +71,9 @@ namespace Hexabell
             int index = indexFromButton[taskButton];
 
             MessageBox.Show("ChangeTaskOptions");       // Debug
+            viewModel.HexagonSize -= 5;                           //
             // Make viewModel.ChangeTaskOptions(index)
         }
-
-        #region [ Changing UI size ]
-        private int hexagonSize = 20;
-        public int HexagonSize
-        {
-            get => hexagonSize;
-            private set
-            {
-                if (value != hexagonSize)
-                    hexagonSize = ValidValue(value, 20, 70);
-
-                HexagonInterval = HexagonInterval;
-                ChangeHexagonPoints(hexagonSize);
-                ChangeMainWindowSize(hexagonSize, HexagonInterval);
-                OnPropertyChanged();
-
-                void ChangeHexagonPoints(int size)
-                {
-                    HexagonPoints = GetHexagonPoints();
-                    BasicPolygon.Points = HexagonPoints;
-
-                    PointCollection GetHexagonPoints()
-                    {
-                        PointCollection points = new PointCollection();
-
-                        CalculatePoints();
-
-                        return points;
-
-                        void CalculatePoints()
-                        {
-                            points.Add(new Point(size, 0));
-                            points.Add(new Point(3 * size, 0));
-                            points.Add(new Point(4 * size, 1.73206 * size));
-                            points.Add(new Point(3 * size, 3.46412 * size));
-                            points.Add(new Point(size, 3.46412 * size));
-                            points.Add(new Point(0, 1.73206 * size));
-                        }
-                    }
-                }
-            }
-        }
-
-        private int hexagonInterval = 0;
-        private int HexagonInterval
-        {
-            get => hexagonInterval;
-            set
-            {
-                if (value != hexagonInterval)
-                    hexagonInterval = ValidValue(value, 0, 50);
-
-                ChangeHexagonPivots(HexagonSize, hexagonInterval);
-                ChangeMainWindowSize(HexagonSize, hexagonInterval);
-                OnPropertyChanged();
-
-                void ChangeHexagonPivots(int size, int interval)
-                {
-                    int xIndex = basicPolygonGridPosition.XIndex;
-                    int yIndex = basicPolygonGridPosition.YIndex;
-                    Canvas.SetLeft(BasicPolygonGrid, XOffset(xIndex));
-                    Canvas.SetTop(BasicPolygonGrid, YOffset(yIndex));
-
-                    foreach (var taskButton in buttonFromIndex)
-                    {
-                        xIndex = taskButtonGridPositions[taskButton.Key].XIndex;
-                        yIndex = taskButtonGridPositions[taskButton.Key].YIndex;
-                        Canvas.SetLeft(taskButton.Value, XOffset(xIndex));
-                        Canvas.SetTop(taskButton.Value, YOffset(yIndex));
-                    }
-
-                    double XOffset(int xIndex)
-                    {
-                        if (xIndex == 1)
-                        {
-                            return 0;
-                        }
-                        else
-                        {
-                            return (xIndex - 1) * (3 * size + 0.866025 * interval);
-                        }
-                    }
-
-                    double YOffset(int yIndex)
-                    {
-                        if (yIndex == 1)
-                        {
-                            return 0;
-                        }
-                        else
-                        {
-                            return (yIndex - 1) * (1.73206 * size + 0.5 * interval);
-                        }
-                    }
-                }
-            }
-        }
-
-        private int ValidValue(int value, int minValidValue, int maxValidValue)
-        {
-            int validValue;
-
-            if (value < minValidValue)
-            {
-                validValue = minValidValue;
-            }
-            else if (value > maxValidValue)
-            {
-                validValue = maxValidValue;
-            }
-            else
-            {
-                validValue = value;
-            }
-
-            return validValue;
-        }
-
-        private void ChangeMainWindowSize(int size, int interval)
-        {
-            double mainWindowWidth = 10 * size + 1.73205d * interval;
-            double mainWindowHeight = 20.78472d * size + 2 * interval;
-
-            this.Width = mainWindowWidth;
-            this.Height = mainWindowHeight;
-        }
-        #endregion
 
         private bool IsNoError()
         {
@@ -355,34 +137,6 @@ namespace Hexabell
         #endregion
 
         private void BasicPolygon_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
-
-        public struct GridPosition
-        {
-            public int XIndex { get; private set; }
-            public int YIndex { get; private set; }
-
-            public GridPosition(int xIndex, int yIndex)
-            {
-                XIndex = ValidValue(xIndex, 1, 100);
-                YIndex = ValidValue(yIndex, 1, 100);
-
-                int ValidValue(int value, int minValidValue, int maxValidValue)
-                {
-                    if (value < minValidValue)
-                    {
-                        return minValidValue;
-                    }
-                    else if (value > maxValidValue)
-                    {
-                        return maxValidValue;
-                    }
-                    else
-                    {
-                        return value;
-                    }
-                }
-            }
-        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- ViewModel.cs
+++ ViewModel.cs
@@ -1,19 +1,428 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Hexabell
 {
-    public class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
-        Model model;
+        private Model model;
+        private readonly int taskQuantity;
+
+        #region [ Hexagon Design Properties ]
+        public double HexagonStrokeThickness { get; private set; } = 3;
+
+        public Brush[] buttonBackgroundColors;
+        public Brush[] ButtonBackgroundColors
+        {
+            get => buttonBackgroundColors;
+            private set
+            {
+                if (buttonBackgroundColors != value)
+                    buttonBackgroundColors = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private Brush[] buttonBorderBrushColors;
+        public Brush[] ButtonBorderBrushColors
+        {
+            get => buttonBorderBrushColors;
+            private set
+            {
+                if (buttonBorderBrushColors != value)
+                    buttonBorderBrushColors = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public Brush DisabledTaskButtonBackground { get; } = new SolidColorBrush(Color.FromArgb(93, 135, 206, 250));       // LightSkyBlue
+        public Brush DisabledTaskButtonBorderBrush { get; } = new SolidColorBrush(Color.FromArgb(120, 135, 206, 250));       // LightSkyBlue
+        public Brush EnabledTaskButtonBackground { get; } = new SolidColorBrush(Color.FromArgb(93, 255, 182, 193));        // LightPink
+        public Brush EnabledTaskButtonBorderBrush { get; } = new SolidColorBrush(Color.FromArgb(120, 255, 182, 193));         // LightPink
+        public Brush IsMouseOverTaskButtonBorderBrush { get; } = new SolidColorBrush(Color.FromArgb(200, 135, 206, 250));         // LightSkyBlue
+        public Brush BasicPolygonBackground { get; } = new SolidColorBrush(Color.FromArgb(93, 135, 206, 250));       // LightSkyBlue
+        public Brush BasicPolygonBorderBrush { get; } = new SolidColorBrush(Color.FromArgb(120, 135, 206, 250));       // LightSkyBlue
+        #endregion
+
+        #region [ Hexagon Shape and Location ]
+        private PointCollection hexagonPoints = new PointCollection
+        {
+            { new Point(50, 0) },
+            { new Point(150, 0) },
+            { new Point(200, 87) },
+            { new Point(150, 173) },
+            { new Point(50, 173) },
+            { new Point(0, 87) },
+        };
+        public PointCollection HexagonPoints
+        {
+            get => hexagonPoints;
+            private set
+            {
+                hexagonPoints = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Point[] taskButtonPivots = new Point[]
+        {
+            new Point(0, 107),
+            new Point(185, 0),
+            new Point(370, 107),
+            new Point(370, 320),
+            new Point(185, 426),
+            new Point(0, 320),
+        };
+        public Point[] TaskButtonPivots
+        {
+            get => taskButtonPivots;
+            private set
+            {
+                taskButtonPivots = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Point basicPolygonPivot = new Point(185, 213);
+        public Point BasicPolygonPivot
+        {
+            get => basicPolygonPivot;
+            private set
+            {
+                basicPolygonPivot = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GridPosition BasicPolygonGridPosition { get; private set; } = new GridPosition(2, 3);
+        public GridPosition[] TaskButtonGridPositions { get; private set; } = new GridPosition[]
+        {
+            new GridPosition(2, 5),
+            new GridPosition(1, 4),
+            new GridPosition(1, 2),
+            new GridPosition(2, 1),
+            new GridPosition(3, 2),
+            new GridPosition(3, 4)
+        };
+        #endregion
+
+        #region [ Size Changing ]
+        private static readonly int minHexagonSize = 20;
+        private static readonly int maxHexagonSize = 70;
+        private int hexagonSize = minHexagonSize;
+        public int HexagonSize
+        {
+            get => hexagonSize;
+            set
+            {
+                if (value != hexagonSize)
+                    hexagonSize = ValidValue(value, minHexagonSize, maxHexagonSize);
+
+                ChangeHexagonPoints(hexagonSize);
+                UpdateFontSizeAndInterval();
+                OnPropertyChanged();
+
+                void ChangeHexagonPoints(int size)
+                {
+                    HexagonPoints = GetHexagonPoints();
+
+                    PointCollection GetHexagonPoints()
+                    {
+                        PointCollection points = new PointCollection();
+
+                        CalculatePoints();
+
+                        return points;
+
+                        void CalculatePoints()
+                        {
+                            points.Add(new Point(size, 0));
+                            points.Add(new Point(3 * size, 0));
+                            points.Add(new Point(4 * size, 1.73206 * size));
+                            points.Add(new Point(3 * size, 3.46412 * size));
+                            points.Add(new Point(size, 3.46412 * size));
+                            points.Add(new Point(0, 1.73206 * size));
+                        }
+                    }
+                }
+
+                void UpdateFontSizeAndInterval()
+                {
+                    TimeFontSize = TimeFontSize;
+                    HexagonInterval = HexagonInterval;
+                }
+            }
+        }
+
+        private static readonly int minHexagonInterval = 0;
+        private static readonly int maxHexagonInterval = 50;
+        private int hexagonInterval = minHexagonInterval;
+        public int HexagonInterval
+        {
+            get => hexagonInterval;
+            set
+            {
+                if (value != hexagonInterval)
+                    hexagonInterval = ValidValue(value, minHexagonInterval, maxHexagonInterval);
+
+                ChangeHexagonPivots(HexagonSize, hexagonInterval);
+                CalculateWindowSize(HexagonSize, hexagonInterval);
+                OnPropertyChanged();
+
+                void ChangeHexagonPivots(int size, int interval)
+                {
+                    int xIndex = BasicPolygonGridPosition.XIndex;
+                    int yIndex = BasicPolygonGridPosition.YIndex;
+                    BasicPolygonPivot = new Point(XOffset(xIndex), YOffset(yIndex));
+
+                    Point[] pivots = new Point[TaskButtonPivots.Length];
+                    for (int i = 0; i < pivots.Length; i++)
+                    {
+                        xIndex = TaskButtonGridPositions[i].XIndex;
+                        yIndex = TaskButtonGridPositions[i].YIndex;
+                        pivots[i] = new Point(XOffset(xIndex), YOffset(yIndex));
+                    }
+                    TaskButtonPivots = pivots;
+
+                    double XOffset(int xIndex)
+                    {
+                        if (xIndex == 1)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return (xIndex - 1) * (3 * size + 0.866025 * interval);
+                        }
+                    }
+
+                    double YOffset(int yIndex)
+                    {
+                        if (yIndex == 1)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return (yIndex - 1) * (1.73206 * size + 0.5 * interval);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static readonly double minTimeFontSizeScale = 0.6;
+        private static readonly double maxTimeFontSizeScale = 1.3;
+        private double timeFontSizeScale = minTimeFontSizeScale;
+        private double TimeFontSizeScale
+        {
+            get => timeFontSizeScale;
+            set
+            {
+                if (timeFontSizeScale != value)
+                    timeFontSizeScale = ValidValue(value, minTimeFontSizeScale, maxTimeFontSizeScale);
+
+                TimeFontSize = TimeFontSize;
+            }
+        }
+
+        public double TimeFontSize
+        {
+            get => HexagonSize * TimeFontSizeScale;
+            private set => OnPropertyChanged();
+        }
+
+        private void CalculateWindowSize(int size, int interval)
+        {
+            double windowWidth = 10 * size + 1.73205d * interval;
+            double windowHeight = 10.39236d * size + 2 * interval;
+
+            if (windowWidth != WindowWidth)
+                WindowWidth = windowWidth;
+
+            if (windowHeight != WindowHeight)
+                WindowHeight = windowHeight;
+        }
+
+        private double windowWidth;
+        public double WindowWidth
+        {
+            get => windowWidth;
+            private set
+            {
+                windowWidth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double windowHeight;
+        public double WindowHeight
+        {
+            get => windowHeight;
+            private set
+            {
+                windowHeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int ValidValue(int value, int minValidValue, int maxValidValue)
+        {
+            int validValue;
+
+            if (value < minValidValue)
+            {
+                validValue = minValidValue;
+            }
+            else if (value > maxValidValue)
+            {
+                validValue = maxValidValue;
+            }
+            else
+            {
+                validValue = value;
+            }
+
+            return validValue;
+        }
+
+        private double ValidValue(double value, double minValidValue, double maxValidValue)
+        {
+            double validValue;
+
+            if (value < minValidValue)
+            {
+                validValue = minValidValue;
+            }
+            else if (value > maxValidValue)
+            {
+                validValue = maxValidValue;
+            }
+            else
+            {
+                validValue = value;
+            }
+
+            return validValue;
+        }
+        #endregion
 
         public bool[] IsTaskEnabled { get => model.IsTaskEnabled; }
 
-        public ViewModel(int taskQuantity) => model = new Model(taskQuantity);
+        public ViewModel(int taskQuantity)
+        {
+            this.taskQuantity = taskQuantity;
+            model = new Model(taskQuantity);
+            model.PropertyChanged += ModelNotify;
+
+            InitializeSizes();
+            InitializeTaskButtonIndexesAndColors();
+            LoadSettings();
+
+            void InitializeSizes() => HexagonSize = HexagonSize;
+
+            void InitializeTaskButtonIndexesAndColors()
+            {
+                ButtonBackgroundColors = new Brush[taskQuantity];
+                ButtonBorderBrushColors = new Brush[taskQuantity];
+                SetTaskButtonColor();
+            }
+
+            void LoadSettings()                 // From settings
+            {
+                HexagonSize = 10;
+                HexagonInterval = 20;
+                TimeFontSizeScale = 0.9;
+            }
+        }
 
         public void ChangeTaskState(int taskIndex) => model.ChangeTaskState(taskIndex);
+
+        private void ModelNotify(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(model.IsTaskEnabled))
+                SetTaskButtonColor();
+        }
+
+        private void SetTaskButtonColor()
+        {
+            for (int i = 0; i < taskQuantity; i++)
+            {
+                ButtonBackgroundColors[i] = GetBackgroundColor(model.IsTaskEnabled[i]);
+                ButtonBorderBrushColors[i] = GetBorderBrushColor(model.IsTaskEnabled[i]);
+            }
+
+            ColorsChangedNotify();
+
+            Brush GetBackgroundColor(bool isTaskEnabled)
+            {
+                if (isTaskEnabled)
+                {
+                    return EnabledTaskButtonBackground;
+                }
+                else
+                {
+                    return DisabledTaskButtonBackground;
+                }
+            }
+
+            Brush GetBorderBrushColor(bool isTaskEnabled)
+            {
+                if (isTaskEnabled)
+                {
+                    return EnabledTaskButtonBorderBrush;
+                }
+                else
+                {
+                    return DisabledTaskButtonBorderBrush;
+                }
+            }
+
+            void ColorsChangedNotify()
+            {
+                ButtonBackgroundColors = ButtonBackgroundColors;
+                ButtonBorderBrushColors = ButtonBorderBrushColors;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public struct GridPosition
+        {
+            public int XIndex { get; private set; }
+            public int YIndex { get; private set; }
+
+            public GridPosition(int xIndex, int yIndex)
+            {
+                XIndex = ValidValue(xIndex, 1, 100);
+                YIndex = ValidValue(yIndex, 1, 100);
+
+                int ValidValue(int value, int minValidValue, int maxValidValue)
+                {
+                    if (value < minValidValue)
+                    {
+                        return minValidValue;
+                    }
+                    else if (value > maxValidValue)
+                    {
+                        return maxValidValue;
+                    }
+                    else
+                    {
+                        return value;
+                    }
+                }
+            }
+        }
     }
 }
